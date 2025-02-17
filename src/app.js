ce1a19ce7d7d674611e9c4892cbe0536c9f26913
Your Name <you@example.com>
2025-02-17 16:29:20 +0100

    minio exercies general

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ require('dotenv').config({ path: require('path').resolve(__dirname, '../.env') }
 const express = require('express');
 const web3 = require('web3').default;
 const app = express();
+const Minio = require('minio');
 const port = process.env.PORT || 5000;
 
 const WEB3_PROVIDER = "https://eth.llamarpc.com"
@@ -23,6 +24,15 @@ app.get('/balance/:address', async (req, res) => {
         res.status(500).json({ error: 'Error fetching balance', details: error.message });
     }
 
+});
+
+
+const minioClient = new Minio.Client({
+    endPoint: "localhost",
+    port: 9009,
+    useSSL: false,
+    accessKey: "minioadmin",
+    secretKey: "minioadmin"
 });
 
 app.listen(port, () => {
