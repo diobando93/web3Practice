2f16e1a5eb2292629abe90e8e81b31954f98e0fb
diobando <devisrael93@gmail.com>
2025-02-19 20:52:51 +0100

    static files and MInIO upload

--- src/app.js
+++ src/app.js
@@ -2,12 +2,19 @@ require('dotenv').config({ path: require('path').resolve(__dirname, '../.env') }
 const express = require('express');
 const web3 = require('web3').default;
 const app = express();
+const path = require('path');
+const fileUpload = require('express-fileupload');
 const Minio = require('minio');
 const port = process.env.PORT || 5000;
 
 const WEB3_PROVIDER = "https://eth.llamarpc.com"
 const web = new web3(WEB3_PROVIDER);
-
+//midlleware for see static files like html
+app.use(express.static("public"));
+//ficheros
+app.use(fileUpload({
+    limits: { fileSize: 50 * 1024 * 1024 }, //50mb
+}));
 app.use(express.json());
 
 const minioClient = new Minio.Client({
@@ -37,6 +44,23 @@ app.get('/balance/:address', async (req, res) => {
 app.post('/minio/createBucket', async (req, res) => {
     try{
         await minioClient.makeBucket(req.body.name, "us-east-1");
+        res.status(200).send({result:"ok"});
+    }catch(error){
+        res.status(500).send({error:error});
+    }
+    
+});
+
+app.post('/minio/addFile', async (req, res) => {
+    const bucket = req.body.bucket;
+    const file = req.files.ficheros;
+    try{
+        await minioClient.putObject(
+            bucket,
+            file.name,
+            file.data,
+            file.data.length,
+        );
         res.status(200).send({result:"ok"});
     }catch(error){
         res.status(500).send({error:error});
