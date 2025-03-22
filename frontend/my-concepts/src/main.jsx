ff5e7610c63b801e9fa827ad94056919d2a504a8
diobando <devisrael93@gmail.com>
2025-03-22 19:01:44 +0100

    Conditionals

--- frontend/my-concepts/src/main.jsx
+++ frontend/my-concepts/src/main.jsx
@@ -6,18 +6,12 @@ const farr = () => [1, "f", [2, 3]]
 const C2 = () => "componente c2"
 const C1 = (props) => <p> <C2/> este es un componente {props.a} </p>
 const lista = ["Madrid", "Barcelona", "Valencia"]
+const sesion = null
 
 
 createRoot(document.getElementById('root')).render(
-  <h1>
-    {2 + 4} {funlo()}
-    {farr()}
-    <C1 a= {25 + 20} />
-    {C1({a:33})}
-    <ul>
-      {lista.map((item, index) => (
-        <li key={index}>{item}</li>
-      ))}
-    </ul>
-  </h1>
+  <div>
+    {sesion && <p>{sesion.usuario}</p>}
+    {sesion ? <p> {sesion.usuario} </p> : <p> no hay sesión </p>}
+  </div>
 )
