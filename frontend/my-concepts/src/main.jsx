c3d3040e7c56d6aa3d0610040797e7c8f00b4f06
diobando <devisrael93@gmail.com>
2025-03-22 19:38:54 +0100

    hooks state

--- frontend/my-concepts/src/main.jsx
+++ frontend/my-concepts/src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import React, { useState } from 'react';
 
 const funlo = () => "todo"
 const farr = () => [1, "f", [2, 3]]
@@ -8,10 +9,36 @@ const C1 = (props) => <p> <C2/> este es un componente {props.a} </p>
 const lista = ["Madrid", "Barcelona", "Valencia"]
 const sesion = null
 
+function Likes(props){
+  const [likes, setLikes] = useState(0);
+  console.log("render", likes);
+  function increment(){
+    setLikes(likes + 1);
+  }
+  return <button onClick={() => increment()}> Likes {likes} </button>
+}
+
 
 createRoot(document.getElementById('root')).render(
   <div>
-    {sesion && <p>{sesion.usuario}</p>}
-    {sesion ? <p> {sesion.usuario} </p> : <p> no hay sesión </p>}
+    <Likes/>
   </div>
 )
+
+
+
+
+/*
+function Likes(props){
+  //let likes = 0;
+  const [likes, setLikes] = useState(0);
+  console.log("render", likes);
+  function increment(){
+    //likes++;
+    //console.log(likes);
+    setLikes(likes + 1);
+  }
+  return <button onClick={() =>increment()}> Likes {likes} </button>
+}
+
+*/
