6f19c3ea39eceb7643ceb779ddf9898f298be842
diobando <devisrael93@gmail.com>
2025-04-14 20:55:20 +0200

    Setup communication front/back

--- faucet/back/index.ts
+++ faucet/back/index.ts
@@ -1,13 +1,16 @@
 import express from 'express';
 import { Request, Response } from 'express';
+import cors from 'cors';
 
 const app = express();
 app.use(express.json());
+app.use(cors());
+
 const port = 3333;
 
 app.get('/:p1/:p2', (req: Request, res: Response) => {
     const { p1, p2 } = req.params;
-    res.send('Hello World! ' + p1 + ' ' + p2);
+    res.send({p1:p1, p2:p2});
 });
 
 app.post( '/', (req: Request, res: Response) => {
--- faucet/front/src/App.tsx
+++ faucet/front/src/App.tsx
@@ -1,11 +1,28 @@
-import { Button } from "@/components/ui/button"
+import { useEffect, useState } from "react";  
 
-function App() {
-  return (
-    <div className="flex flex-col items-center justify-center min-h-svh">
-      <Button>Click me</Button>
-    </div>
-  )
+type Data = {
+  p1: string;
+  p2: string;
 }
 
-export default App
+export default function App() {
+
+  const [data, setData] = useState <Data | null>(null);
+
+  useEffect( () => {
+
+    fetch( "http://localhost:3333/112/34543" )
+      .then( ( response ) => response.json() )
+      .then( ( data => setData(data) ) )
+
+  }, [] )
+
+  if(!data) return <div>Loading...</div>
+
+  return (
+    <div>
+      {data.p1} {data.p2}
+    </div>
+  )
+
+}
